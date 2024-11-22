cc82539e66a4f3a74e8999e1d1ec86810fccca92
Devaj Shourya Bhogireddi <146690147+MarchEgg@users.noreply.github.com>
2024-11-22 09:56:33 -0600

    Added optional paramater

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -474,6 +474,9 @@ public class MecanumDrive {
     }
 
     public TrajectoryActionBuilder actionBuilder(Pose2d beginPose) {
+        TrajectoryActionBuilder(beginPose, 0.0);
+    }
+    public TrajectoryActionBuilder actionBuilder(Pose2d beginPose, double finalVel) {
         return new TrajectoryActionBuilder(
                 TurnAction::new,
                 FollowTrajectoryAction::new,
@@ -483,7 +486,7 @@ public class MecanumDrive {
                                 0.25, 0.1, 1e-2
                         )
                 ),
-                beginPose, 0.0,
+                beginPose, finalVel,
                 defaultTurnConstraints,
                 defaultVelConstraint, defaultAccelConstraint
         );
